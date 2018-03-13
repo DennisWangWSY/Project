fb701f7ee7470961bc85f985a9eb55cb2e994802
DanielWang <sywang@tju.edu.cn>
2018-03-13 21:35:04 +0800

    Change Style
    Change the style of the new quiz form

--- db.php
+++ db.php
@@ -24,9 +24,10 @@ $k = 0;
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+    <link href="css/newquiz.css" rel="stylesheet">
     <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
     <script src="js/simpleajax.js"></script>
-	<script src="js/db.js"></script>
+    <script src="js/db.js"></script>
 
 
 </head>
@@ -35,22 +36,22 @@ $k = 0;
 
     <div id="wrapper">
         <div id="page-wrapper" class="gray-bg">
-        <div>
-            <span id="dbhead1">Welcome to Quize Home.     </span>
-            <div id="logout">
-            	<a href="my.php">
-                <i id="dbhead2" class="fa fa-sign-out"></i><span id="dbhead2">    Home</span>
-            	</a>
+            <div>
+                <span id="dbhead1">Welcome to Quize Home.     </span>
+                <div id="logout">
+                 <a href="my.php">
+                    <i id="dbhead2" class="fa fa-sign-out"></i><span id="dbhead2">    Home</span>
+                </a>
             </div>    
         </div>
-            <div id="dipth">
-                <div class="col-lg-10">  
-                </div>
+        <div id="dipth">
+            <div class="col-lg-10">  
             </div>
+        </div>
 
         <div class="wrapper">
             <div class="row">
-            	<div id="left" class="col-lg-4">
+            	<div id="left" class="col-lg-6">
             		<?php
             		for($i = 1; $i <= $leftcount; $i++){
             			$quizContent = file($quizs[$k], FILE_IGNORE_NEW_LINES); $k++;?>
@@ -77,7 +78,7 @@ $k = 0;
             		}
             		?>
             	</div>
-                <div id="right" class="col-lg-4">
+                <div id="right" class="col-lg-6">
                 	<?php
                 	for($i = $rightcount; $i <= count($quizs); $i++){
                 		$quizContent = file($quizs[$k], FILE_IGNORE_NEW_LINES); $k++;?>
@@ -103,29 +104,68 @@ $k = 0;
                 		<?php
                 	}
                 	?>
+                </div>
             </div>
-            <div class="col-lg-4">
-            	<div id="newquizdiv">
-            		<form id="newQuiz" class="form-horizontal">
-            			<div class="form-group"><label class="col-sm-2 control-label">Quiz:</label ><div class="col-sm-10"><textarea name="content"></textarea></div></div>
-            			<div class="form-group"><label class="col-sm-2 control-label">Domain:</label><div class="col-sm-10"> <input name="domain" class="domain" /></div></div>
-            			<div class="form-group"><label class="col-sm-2 control-label">Diff:</label><div class="col-sm-10"><input name="difficulty" type="range" min="1" max="10" step="1" value="1" class="difficulty" /></div></div>
-            			<div class="form-group"><label class="col-sm-2 control-label">Choice A:</label><div class="col-sm-10"><input name="A" class="choice" /></div></div>
-            			<div class="form-group"><label class="col-sm-2 control-label">Choice B:</label><div class="col-sm-10"><input name="B" class="choice" /></div></div>
-            			<div class="form-group"><label class="col-sm-2 control-label">Choice C:</label><div class="col-sm-10"><input name="C" class="choice" /></div></div>
-            			<div class="form-group"><label class="col-sm-2 control-label">Choice D:</label><div class="col-sm-10"><input name="D" class="choice" /></div></div>
-            			<div class="form-group"><label class="col-sm-2 control-label">Answer:</label><div class="col-sm-10">
-            			<div><label>A<input type="checkbox" name="choice" value="A" /></label>
-            				<label>B<input type="checkbox" name="choice" value="B" /></label>
-            				<label>C<input type="checkbox" name="choice" value="C" /></label>
-            				<label>D<input type="checkbox" name="choice" value="D" /></label>
-            			</div></div></div>
-            			<input id="addquizButton" class="btn btn-info" type="submit" value="Add quiz" title="add a new quiz"/>
-            		</form>
-            	</div>
-            	
-            </div>
+            <div id="newquizdiv">
+                <!-- <form id="newQuiz" class="form-horizontal">
+                    <div class="form-group"><label class="col-sm-2 control-label">Quiz:</label ><div class="col-sm-10"><textarea name="content"></textarea></div></div>
+                    <div class="form-group"><label class="col-sm-2 control-label">Domain:</label><div class="col-sm-10"> <input name="domain" class="domain" /></div></div>
+                    <div class="form-group"><label class="col-sm-2 control-label">Diff:</label><div class="col-sm-10"><input name="difficulty" type="range" min="1" max="10" step="1" value="1" class="difficulty" /></div></div>
+                    <div class="form-group"><label class="col-sm-2 control-label">Choice A:</label><div class="col-sm-10"><input name="A" class="choice" /></div></div>
+                    <div class="form-group"><label class="col-sm-2 control-label">Choice B:</label><div class="col-sm-10"><input name="B" class="choice" /></div></div>
+                    <div class="form-group"><label class="col-sm-2 control-label">Choice C:</label><div class="col-sm-10"><input name="C" class="choice" /></div></div>
+                    <div class="form-group"><label class="col-sm-2 control-label">Choice D:</label><div class="col-sm-10"><input name="D" class="choice" /></div></div>
+                    <div class="form-group"><label class="col-sm-2 control-label">Answer:</label>
+                        <div class="col-sm-10">
+                            <div><label>A<input type="checkbox" name="choice" value="A" /></label>
+                             <label>B<input type="checkbox" name="choice" value="B" /></label>
+                             <label>C<input type="checkbox" name="choice" value="C" /></label>
+                             <label>D<input type="checkbox" name="choice" value="D" /></label>
+                         </div>
+                     </div>
+                 </div>
+                 <input id="addquizButton" class="btn btn-info" type="submit" value="Add quiz" title="add a new quiz"/>
+             </form> -->
+             <div id="contact-form" class="clearfix">
+                <h1>Add new Quiz</h1>
+                <h2>Fill in the form here to add your new quiz to our Quiz Database!</h2>
+                <form method="post">
+                    <label for="content">Quiz Content: <span class="required">*</span></label>
+                    <textarea name="content" placeholder="Your quiz content must be greater than 20 charcters" required="required" data-minlength="20"></textarea>
+                    <label for="Domain">Domain: <span class="required">*</span></label>
+                    <select name="domain">
+                        <option value="General">General</option>
+                        <option value="History">History</option>
+                        <option value="Sports">Sports</option>
+                        <option value="Literature">Literature</option>
+                        <option value="Geography">Geography</option>
+                        <option value="Architecture">Architecture</option>
+                        <option value="Arts">Arts</option>
+                        <option value="IT">IT</option>
+                        <option value="Entertainment">Entertainment</option>
+                        <option value="Biology">Biology</option>
+                        <option value="Maths">Maths</option>
+                    </select>
+                    <label for="difficulty">Difficulty: <span class="required">*</span></label>
+                    <input name="difficulty" type="range" min="1" max="10" step="1" value="1" class="difficulty" />
 
+                    <label for="domain">Choice A: <span class="required">*</span><input type="checkbox" name="choice" value="A" /></label>
+                    <input type="text" name="A" value="" placeholder="Choice A" required="required" />
+
+                    <label for="domain">Choice B: <span class="required">*</span><input type="checkbox" name="choice" value="B" /></label>
+                    <input type="text" name="B" value="" placeholder="Choice B" required="required" />
+
+                    <label for="domain">Choice C: <span class="required">*</span><input type="checkbox" name="choice" value="C" /></label>
+                    <input type="text" name="C" value="" placeholder="Choice C" required="required" />
+
+                    <label for="domain">Choice D: <span class="required">*</span><input type="checkbox" name="choice" value="D" /></label>
+                    <input type="text" name="D" value="" placeholder="Choice D" required="required" />
+
+                    <input id="submit-button" type="submit" value="Add quiz" />
+                    <p id="req-field-desc"><span class="required">*</span> indicates a required field</p>
+                    <p id="req-field-desc"> Check the correct answers of your quiz!</p>
+                </form>
+            </div>
         </div>
         <div class="footer">
             <div class="pull-right">
@@ -135,21 +175,20 @@ $k = 0;
                 <strong>Copyright</strong> Example Company &copy; 2014-2015
             </div>
         </div>
-
-        </div>
-        </div>
-
+    </div>
+</div>
 
 
-    <!-- Mainly scripts -->
-    <script src="js/jquery-2.1.1.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
-    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 
-    <!-- Custom and plugin javascript -->
-    <script src="js/inspinia.js"></script>
-    <script src="js/plugins/pace/pace.min.js"></script>
+<!-- Mainly scripts -->
+<script src="js/jquery-2.1.1.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+<!-- Custom and plugin javascript -->
+<script src="js/inspinia.js"></script>
+<script src="js/plugins/pace/pace.min.js"></script>
 
 </body>
 
