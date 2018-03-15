824537fa93229fc3c38490abca605b31fd984c5d
DanielWang <sywang@tju.edu.cn>
2018-03-15 20:21:20 +0800

    my.php hints

--- db.php
+++ db.php
@@ -47,7 +47,7 @@ $k = 0;
             <div>
                 <span id="dbhead1">Welcome to Quize Home.     </span>
                 <div id="logout">
-                   <a href="my.php">
+                 <a href="my.php">
                     <i id="dbhead2" class="fa fa-sign-out"></i><span id="dbhead2">    Home</span>
                 </a>
             </div>    
@@ -57,107 +57,104 @@ $k = 0;
             </div>
         </div>
 
-        <div class="wrapper">
-            <div id="newquizdiv">
-                    <div id="contact-form" class="clearfix">
-                        <h1>Add new Quiz</h1>
-                        <h2>Fill in the form here to add your new quiz to our Quiz Database!</h2>
-                        <form method="post">
-                            <label for="content">Quiz Content: <span class="required">*</span></label>
-                            <textarea name="content" placeholder="Your quiz content must be greater than 20 charcters" required="required" data-minlength="20"></textarea>
-                            <label for="Domain">Domain: <span class="required">*</span></label>
-                            <select name="domain">
-                                <option value="General">General</option>
-                                <option value="History">History</option>
-                                <option value="Sports">Sports</option>
-                                <option value="Literature">Literature</option>
-                                <option value="Geography">Geography</option>
-                                <option value="Architecture">Architecture</option>
-                                <option value="Arts">Arts</option>
-                                <option value="IT">IT</option>
-                                <option value="Entertainment">Entertainment</option>
-                                <option value="Biology">Biology</option>
-                                <option value="Maths">Maths</option>
-                            </select>
-                            <label for="difficulty">Difficulty: <span class="required">*</span></label>
-                            <input name="difficulty" type="range" min="1" max="10" step="1" value="1" class="difficulty" />
+        <div class="wrapper" id="newquizdiv">
+            <div id="contact-form" class="clearfix">
+                <h1>Add new Quiz</h1>
+                <h2>Fill in the form here to add your new quiz to our Quiz Database!</h2>
+                <form method="post">
+                    <label for="content">Quiz Content: <span class="required">*</span></label>
+                    <textarea name="content" placeholder="Your quiz content must be greater than 20 charcters" required="required" data-minlength="20"></textarea>
+                    <label for="Domain">Domain: <span class="required">*</span></label>
+                    <select name="domain">
+                        <option value="General">General</option>
+                        <option value="History">History</option>
+                        <option value="Sports">Sports</option>
+                        <option value="Literature">Literature</option>
+                        <option value="Geography">Geography</option>
+                        <option value="Architecture">Architecture</option>
+                        <option value="Arts">Arts</option>
+                        <option value="IT">IT</option>
+                        <option value="Entertainment">Entertainment</option>
+                        <option value="Biology">Biology</option>
+                        <option value="Maths">Maths</option>
+                    </select>
+                    <label for="difficulty">Difficulty: <span class="required">*</span></label>
+                    <input name="difficulty" type="range" min="1" max="10" step="1" value="1" class="difficulty" />
+                    <label for="domain">Choice A: <span class="required">*</span><input type="checkbox" name="choice" value="A" /></label>
+                    <input type="text" name="A" value="" placeholder="Choice A" required="required" />
 
-                            <label for="domain">Choice A: <span class="required">*</span><input type="checkbox" name="choice" value="A" /></label>
-                            <input type="text" name="A" value="" placeholder="Choice A" required="required" />
+                    <label for="domain">Choice B: <span class="required">*</span><input type="checkbox" name="choice" value="B" /></label>
+                    <input type="text" name="B" value="" placeholder="Choice B" required="required" />
 
-                            <label for="domain">Choice B: <span class="required">*</span><input type="checkbox" name="choice" value="B" /></label>
-                            <input type="text" name="B" value="" placeholder="Choice B" required="required" />
+                    <label for="domain">Choice C: <span class="required">*</span><input type="checkbox" name="choice" value="C" /></label>
+                    <input type="text" name="C" value="" placeholder="Choice C" required="required" />
 
-                            <label for="domain">Choice C: <span class="required">*</span><input type="checkbox" name="choice" value="C" /></label>
-                            <input type="text" name="C" value="" placeholder="Choice C" required="required" />
+                    <label for="domain">Choice D: <span class="required">*</span><input type="checkbox" name="choice" value="D" /></label>
+                    <input type="text" name="D" value="" placeholder="Choice D" required="required" />
 
-                            <label for="domain">Choice D: <span class="required">*</span><input type="checkbox" name="choice" value="D" /></label>
-                            <input type="text" name="D" value="" placeholder="Choice D" required="required" />
-
-                            <input id="submit-button" type="submit" value="Add quiz" />
-                            <p id="req-field-desc"><span class="required">*</span> indicates a required field</p>
-                            <p id="req-field-desc"> Check the correct answers of your quiz!</p>
+                    <input id="submit-button" type="submit" value="Add quiz" />
+                    <p id="req-field-desc"><span class="required">*</span> indicates a required field</p>
+                    <p id="req-field-desc"> Check the correct answers of your quiz!</p>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+           <div id="left" class="col-lg-6">
+              <?php
+              for($i = 1; $i <= $leftcount; $i++){
+                 $quizContent = file($quizs[$k], FILE_IGNORE_NEW_LINES); $k++;?>
+                 <div class="ibox" quiz_id="<?= $quizs[$k-1]; ?>">
+                    <div class="ibox-content">
+                       <div id="content">
+                          <form class="list left" quiz_id="<?= $quizs[$k-1]; ?>">	
+                             <div class="quiz_title">
+                                <?= quiz_domain($quizContent);?> -- Difficulty: <?= quiz_difficulty($quizContent);?>
+                                <input id="delete_quiz" class="btn btn-warning btn-sm" type="submit" name="delete_quiz" value="X" quiz_id="<?= $quizs[$k-1]; ?>" title="delete this quiz"/>
+                            </div>	
+                            <ul>
+                                <li><span class="todo"><?= quiz_content($quizContent);?></span></li>
+                                <li><span class="todo"><?= quiz_choicesA($quizContent);?></span></li>
+                                <li><span class="todo"><?= quiz_choicesB($quizContent);?></span></li>
+                                <li><span class="todo"><?= quiz_choicesC($quizContent);?></span></li>
+                                <li><span class="todo"><?= quiz_choicesD($quizContent);?></span></li>
+                            </ul>
                         </form>
                     </div>
                 </div>
             </div>
-            <div class="row">
-            	<div id="left" class="col-lg-6">
-            		<?php
-            		for($i = 1; $i <= $leftcount; $i++){
-            			$quizContent = file($quizs[$k], FILE_IGNORE_NEW_LINES); $k++;?>
-            			<div class="ibox" quiz_id="<?= $quizs[$k-1]; ?>">
-            				<div class="ibox-content">
-            					<div id="content">
-            						<form class="list left" quiz_id="<?= $quizs[$k-1]; ?>">	
-            							<div class="quiz_title">
-            								<?= quiz_domain($quizContent);?> -- Difficulty: <?= quiz_difficulty($quizContent);?>
-            								<input id="delete_quiz" class="btn btn-warning btn-sm" type="submit" name="delete_quiz" value="X" quiz_id="<?= $quizs[$k-1]; ?>" title="delete this quiz"/>
-            							</div>	
-            							<ul>
-            								<li><span class="todo"><?= quiz_content($quizContent);?></span></li>
-            								<li><span class="todo"><?= quiz_choicesA($quizContent);?></span></li>
-            								<li><span class="todo"><?= quiz_choicesB($quizContent);?></span></li>
-            								<li><span class="todo"><?= quiz_choicesC($quizContent);?></span></li>
-            								<li><span class="todo"><?= quiz_choicesD($quizContent);?></span></li>
-            							</ul>
-            						</form>
-            					</div>
-            				</div>
-            			</div>
-            			<?php
-            		}
-            		?>
-            	</div>
-                <div id="right" class="col-lg-6">
-                	<?php
-                	for($i = $rightcount; $i <= count($quizs); $i++){
-                		$quizContent = file($quizs[$k], FILE_IGNORE_NEW_LINES); $k++;?>
-                		<div class="ibox" quiz_id="<?= $quizs[$k-1]; ?>">
-                			<div class="ibox-content">
-                				<div id="content">
-                					<form class="list left" quiz_id="<?= $quizs[$k-1]; ?>">	
-                						<div class="quiz_title">
-                							<?= quiz_domain($quizContent);?> -- Difficulty: <?= quiz_difficulty($quizContent);?>
-                							<input id="delete_quiz" class="btn btn-warning btn-sm" class="button right" type="submit" name="delete_quiz" value="X" quiz_id="<?= $quizs[$k-1]; ?>" title="delete this quiz"/>
-                						</div>	
-                						<ul>
-                							<li><span class="todo"><?= quiz_content($quizContent);?></span></li>
-                							<li><span class="todo"><?= quiz_choicesA($quizContent);?></span></li>
-                							<li><span class="todo"><?= quiz_choicesB($quizContent);?></span></li>
-                							<li><span class="todo"><?= quiz_choicesC($quizContent);?></span></li>
-                							<li><span class="todo"><?= quiz_choicesD($quizContent);?></span></li>
-                						</ul>
-                					</form>
-                				</div>
-                			</div>
-                		</div>
-                		<?php
-                	}
-                	?>
-                </div>
-            </div>
+            <?php
+        }
+        ?>
     </div>
+    <div id="right" class="col-lg-6">
+       <?php
+       for($i = $rightcount; $i <= count($quizs); $i++){
+          $quizContent = file($quizs[$k], FILE_IGNORE_NEW_LINES); $k++;?>
+          <div class="ibox" quiz_id="<?= $quizs[$k-1]; ?>">
+             <div class="ibox-content">
+                <div id="content">
+                   <form class="list left" quiz_id="<?= $quizs[$k-1]; ?>">	
+                      <div class="quiz_title">
+                         <?= quiz_domain($quizContent);?> -- Difficulty: <?= quiz_difficulty($quizContent);?>
+                         <input id="delete_quiz" class="btn btn-warning btn-sm" class="button right" type="submit" name="delete_quiz" value="X" quiz_id="<?= $quizs[$k-1]; ?>" title="delete this quiz"/>
+                     </div>	
+                     <ul>
+                         <li><span class="todo"><?= quiz_content($quizContent);?></span></li>
+                         <li><span class="todo"><?= quiz_choicesA($quizContent);?></span></li>
+                         <li><span class="todo"><?= quiz_choicesB($quizContent);?></span></li>
+                         <li><span class="todo"><?= quiz_choicesC($quizContent);?></span></li>
+                         <li><span class="todo"><?= quiz_choicesD($quizContent);?></span></li>
+                     </ul>
+                 </form>
+             </div>
+         </div>
+     </div>
+     <?php
+ }
+ ?>
+</div>
+</div>
+</div>
 
 </body>
 
--- my.php
+++ my.php
@@ -24,6 +24,7 @@ $rightcount = $leftcount;
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+    <link href="css/my.css" rel="stylesheet">
     <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
     <script src="js/simpleajax.js"></script>
     <script src="js/db.js"></script>
@@ -48,196 +49,159 @@ $rightcount = $leftcount;
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
                         &times;
                     </button>
-                    <h4 class="modal-title" id="myModalLabel">
-                        题目
-                    </h4>
+                    <h4 class="modal-title" id="myModalLabel"></h4>
                 </div>
-                <div class="modal-body" id = "difficulty">
-                    在这里添加一些文本
-                </div>
-                <div class="modal-body" id = "content">
-                    在这里添加一些文本
-                </div>
-                <div class="modal-body" id = "domain">
-                    在这里添加一些文本
-                </div>
-                <div class="modal-body" id = "a">
-                    在这里添加一些文本
-                </div>
-                <div class="modal-body" id = "b">
-                    在这里添加一些文本
-                </div>
-                <div class="modal-body" id = "c">
-                    在这里添加一些文本
-                </div>
-                <div class="modal-body" id = "d">
-                    在这里添加一些文本
-                </div>
-                <div class="modal-footer">
-               <!-- <button type="button" class="btn btn-default" data-dismiss="modal">关闭
-                </button>
-                
-                <button type="button" class="btn btn-primary">
-                    提交更改
-                </button>-->
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal -->
-</div>
-
-
-
-<div id="wraper">
-    <div id="page-wrapper" class="gray-bg">
-        <div>
-            <span id="dbhead1">
-                <?php  
-
-                //if(isset($_SESSION["login"]))
-                if(true){
-                    print $_SESSION["login"] . "'s Home";
-                }
-                ?>
-            </span>
-
-            <div id="logout">
-
-                <a href="quiz.php" type="button" class="btn-primary">
-                    Start quiz
-                </a>
-
-                <a href="db.php" type="button" class="btn-primary">
-                    Manage quiz
-                </a>
-
-                <a href="logout.php" type="button" class="btn-primary">
-                    Logout
-                </a>
-
+                <div class="modal-body" id = "difficulty"></div>
+                <div class="modal-body" id = "content"></div>
+                <div class="modal-body" id = "domain"></div>
+                <div class="modal-body" id = "a"></div>
+                <div class="modal-body" id = "b"></div>
+                <div class="modal-body" id = "c"></div>
+                <div class="modal-body" id = "d"></div>
+                <div class="modal-footer"></div>
             </div>
         </div>
-        <div id="dipth">
-            <div class="col-lg-10">  
-            </div>
+    </div>
+    <div id="wraper">
+        <div id="page-wrapper" class="gray-bg"><div>
+        <span id="dbhead1">
+        <?php  
+        if(isset($_SESSION["login"]))
+            print $_SESSION["login"] . "'s Home";
+        ?>
+        </span>
+        <div id="logout">
+            <a href="quiz.php" type="button" class="btn-primary">
+                Start quiz
+            </a>
+
+            <a href="db.php" type="button" class="btn-primary">
+                Manage quiz
+            </a>
+
+            <a href="logout.php" type="button" class="btn-primary">
+                Logout
+            </a>
+
+        </div>
+    </div>
+    <div id="dipth">
+        <div class="col-lg-10"></div>
+    </div>
+
+    <div class="wrapper">
+        <div class="wrapper" id="board">
+                <h1>Hints</h1>
+                <h2>Each block on the left is a quiz record</h2>
+                <h2>Click on the quiz id to review the quiz!</h2>
         </div>
 
-        <div class="wrapper">
-            <div class="row">
-             <div id="left" class="col-lg-4">
-              <?php
+        <div class="row">
+            <div id="left" class="col-lg-6">
+            <?php
               for($i = 0; $i < $leftcount; $i++){
                 $recordContent = file($records[$i], FILE_IGNORE_NEW_LINES);
-                ?>
+            ?>
 
                 <div class="ibox">
                     <div class="ibox-content">
-                     <div id="content">
-                      <form class="list left">	
-                       <div class="quiz_title">
-                        Score:
-                        <?= recordScore($recordContent) ?>
+                        <div id="content">
+                            <form class="list left">
+                                <div class="quiz_title">
+                                    Score:
+                                    <?= recordScore($recordContent) ?>
+                                    <input id="delete_quiz" class="btn btn-warning btn-sm" type="submit" name="delete_quiz" value="X" quiz_id="<?= $quizs[$k-1]; ?>" title="delete this quiz"/>
+                                </div>	
 
-                        <input id="delete_quiz" class="btn btn-warning btn-sm" type="submit" name="delete_quiz" value="X" quiz_id="<?= $quizs[$k-1]; ?>" title="delete this quiz"/>
-                    </div>	
+                                <ul>
+                                    <li>Data:<?= basename($records[$i]) ?></li>
+                                    <li>Quiz:
+                                        <?php
+                                        $arr = recordQuiz($recordContent);
 
-                    <ul>
-                        <li>Data:<?= basename($records[$i]) ?></li>
-                        <li>Quiz:
-                            <?php
-                            $arr = recordQuiz($recordContent);
+                                        for($j=0; $j<count($arr); $j++) {
+                                            ?>
+                                            <a class="showmodal" data-target="#myModal" data-toggle="modal">
+                                                <?php
+                                                print $arr[$j] . " ";
+                                            }
+                                            ?>
+                                            </a>
+                                    </li>
+                                    <li>Correct: <?php
+                                    $arr = recordCorrect($recordContent);
+                                    for($j=0; $j<count($arr); $j++) {
+                                            ?>
+                                            <a class="showmodal" data-target="#myModal" data-toggle="modal">
+                                                <?php
+                                                print $arr[$j] . " ";
+                                            }
+                                            ?>
+                                            </a>
+                                    </li>
+                                </ul>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                }
+                ?>
+            </div>
+            <div id="right" class="col-lg-6">
+            <?php
+            for($i = $rightcount; $i < count($records); $i++){
+                $recordContent = file($records[$i], FILE_IGNORE_NEW_LINES);
+            ?>
+                <div class="ibox">
+                    <div class="ibox-content">
+                        <div id="content">
+                            <form class="list left">  
+                                <div class="quiz_title">
+                                    Score:
+                                    <?= recordScore($recordContent) ?>
 
-                            for($j=0; $j<count($arr); $j++) {
-                                ?>
-                                <a class="showmodal" data-target="#myModal" data-toggle="modal">
-                                    <?php
-                                    print $arr[$j] . " ";
-                                }
-                                ?>
-                            </a>
-                            
-                        </li>
-                        <li>Correct: <?php
-                        $arr = recordCorrect($recordContent);
-                        for($j=0; $j<count($arr); $j++) {
-                            print $arr[$j] . " ";
-                        }
-                        ?>
+                                    <input id="delete_quiz" class="btn btn-warning btn-sm" type="submit" name="delete_quiz" value="X" quiz_id="<?= $quizs[$k-1]; ?>" title="delete this quiz"/>
+                                </div>
 
-                    </li>
-                </ul>
+                                <ul>
+                                    <li>Data:<?= basename($records[$i]) ?></li>
+                                    <li>Quiz:
+                                        <?php
+                                        $arr = recordQuiz($recordContent);
 
+                                        for($j=0; $j<count($arr); $j++) {
+                                            ?>
+                                            <a class="showmodal" data-target="#myModal" data-toggle="modal">
+                                                <?php
+                                                print $arr[$j] . " ";
+                                            }
+                                            ?>
+                                        </a>
 
-            </form>
+                                    </li>
+                                    <li>Correct: <?php
+                                    $arr = recordCorrect($recordContent);
+                                    for($j=0; $j<count($arr); $j++) {
+                                            ?>
+                                            <a class="showmodal" data-target="#myModal" data-toggle="modal">
+                                                <?php
+                                                print $arr[$j] . " ";
+                                            }
+                                            ?>
+                                            </a>
+                                    </li>
+                                </ul>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                }
+                ?>
+            </div>
         </div>
     </div>
-</div>
-<?php
-}
-?>
-
-</div>
-<div id="right" class="col-lg-4">
- <?php
- for($i = $rightcount; $i < count($records); $i++){
-
-    $recordContent = file($records[$i], FILE_IGNORE_NEW_LINES);
-    ?>
-
-    <div class="ibox">
-        <div class="ibox-content">
-         <div id="content">
-          <form class="list left">  
-           <div class="quiz_title">
-            Score:
-            <?= recordScore($recordContent) ?>
-
-            <input id="delete_quiz" class="btn btn-warning btn-sm" type="submit" name="delete_quiz" value="X" quiz_id="<?= $quizs[$k-1]; ?>" title="delete this quiz"/>
-        </div>
-
-        <ul>
-            <li>Data:<?= basename($records[$i]) ?></li>
-            <li>Quiz:
-                <?php
-                $arr = recordQuiz($recordContent);
-
-                for($j=0; $j<count($arr); $j++) {
-                    ?>
-                    <a class="showmodal" data-target="#myModal" data-toggle="modal">
-                        <?php
-                        print $arr[$j] . " ";
-                    }
-                    ?>
-                </a>
-                
-            </li>
-            <li>Correct: <?php
-            $arr = recordCorrect($recordContent);
-            for($j=0; $j<count($arr); $j++) {
-                print $arr[$j] . " ";
-            }
-            ?>
-
-        </li>
-    </ul>
-
-
-</form>
-</div>
-</div>
-</div>
-<?php
-}
-?>
-</div>
-
-
-</div>
-
-
-</div>
-</div>
-
-
 </body>
 
 </html>
